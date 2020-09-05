90790546f44b8098e8ffc433c6411a42c42f893b
NunoFernandesofficial <NunoFernandesofficial@users.noreply.github.com>
2020-09-05 21:29:43 +0100

    05/09/2020 commit Changed the buttons background color after the onClickEvent

--- src/main/java/com/example/calculadorabasica/MainActivity.kt
+++ src/main/java/com/example/calculadorabasica/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadorabasica
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     var isNewOp = true
     var oldNumber = ""
     var op = "+"
+    var result = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,40 +27,106 @@ class MainActivity : AppCompatActivity() {
         when (buselect.id) {
             bu1.id -> {
                 buclick += "1"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+
             }
             bu2.id -> {
                 buclick += "2"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
             }
             bu3.id -> {
                 buclick += "3"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
             }
             bu4.id -> {
                 buclick += "4"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
             }
             bu5.id -> {
                 buclick += "5"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
             bu6.id -> {
                 buclick += "6"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+
             }
             bu7.id -> {
                 buclick += "7"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
             bu8.id -> {
                 buclick += "8"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
             bu9.id -> {
                 buclick += "9"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
             bu0.id -> {
                 buclick += "0"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
             buDot.id -> {
                 buclick += "."
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+            }
+            buDot.id -> {
+                buclick += ""
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
             buPlusMinus.id -> {
                 buclick = "-$buclick"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
             }
+            buPi.id -> {
+                buclick += "3.14"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
+
+            }
+
 
         }
         editText.setText(buclick)
@@ -70,70 +138,102 @@ class MainActivity : AppCompatActivity() {
         isNewOp = true
         oldNumber = editText.text.toString()
         var buselect = view as Button
+
         when (buselect.id) {
             buMultiply.id -> {
                 op = "*"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_buttonazul)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
             }
             buPlus.id -> {
                 op = "+"
+                buPlus.setBackgroundResource(R.drawable.my_buttonazul)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
             }
             buMinus.id -> {
                 op = "-"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_buttonazul)
+                buDivide.setBackgroundResource(R.drawable.my_button2)
             }
             buDivide.id -> {
                 op = "/"
+                buPlus.setBackgroundResource(R.drawable.my_button2)
+                buMultiply.setBackgroundResource(R.drawable.my_button2)
+                buMinus.setBackgroundResource(R.drawable.my_button2)
+                buDivide.setBackgroundResource(R.drawable.my_buttonazul)
+            }
+            buEqual.id -> {
+                op = "="
             }
 
 
         }
+
 
 
     }
 
 
+
     fun equalEvent(view: View) {
 
-        var newnumber = editText.text.toString()
-        var result = 0.0
+        var newNumber = editText.text.toString()
+
+        buPlus.setBackgroundResource(R.drawable.my_button2)
+        buMultiply.setBackgroundResource(R.drawable.my_button2)
+        buDivide.setBackgroundResource(R.drawable.my_button2)
+        buMinus.setBackgroundResource(R.drawable.my_button2)
+
         when (op) {
             "+" -> {
-                result = oldNumber.toDouble() + newnumber.toLong()
+                result = oldNumber.toDouble() + newNumber.toDouble()
             }
             "*" -> {
-                result = oldNumber.toDouble() * newnumber.toLong()
-            }
+                result = oldNumber.toDouble() * newNumber.toDouble()
+                }
             "/" -> {
-                result = oldNumber.toDouble() / newnumber.toLong()
+                result = oldNumber.toDouble() / newNumber.toDouble()
             }
             "-" -> {
-                result = oldNumber.toDouble() - newnumber.toLong()
+                result = oldNumber.toDouble() - newNumber.toDouble()
             }
+
+
         }
-        editText.setText(result.toString())
+        editText.setText(result.toString().take(4))
+
     }
 
     fun ApagarEvent(view: View) {
         editText.setText("0")
         isNewOp = true
+        buPlus.setBackgroundResource(R.drawable.my_button2)
+        buMultiply.setBackgroundResource(R.drawable.my_button2)
+        buMinus.setBackgroundResource(R.drawable.my_button2)
+        buDivide.setBackgroundResource(R.drawable.my_button2)
 
 
     }
-
-    fun percentEvent(view: View) {
-
-        var no = editText.text.toString().toDouble() / 100
-        editText.setText(no.toString())
-        isNewOp = true
-
-
-    }
-
-
-
-
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
